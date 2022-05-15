055e791c7eee03344e532e4196da896603b1494e
Jeremy <asirwaadj@gmail.com>
2022-05-15 18:03:52 +0530

    final

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Nav.css";
+import smlogo from "./GCTsm.png";
 
 export const Nav = () => {
 	return (
 		<div className="navbody">
 			<div className="navbar">
 				<div className="navbarcontainer">
+					
 					<h3 className="logotxt">
 						GCT <span className="ss">StartUp Stories</span>
 					</h3>
